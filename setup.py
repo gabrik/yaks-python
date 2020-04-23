664f8339a2da449a7c8a8206643a79ba13323339
gabrik <gabriele.baldoni@gmail.com>
2020-04-23 17:34:11 +0200

    updated version to 0.3.0.post1
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def read(fname):
 
 setup(
     name='yaks',
-    version='0.3.0',
+    version='0.3.0.post1',
     author='ADLINK Advance Technology Office',
     description='Python API to access the YAKS service',
     long_description=read('README.md'),
